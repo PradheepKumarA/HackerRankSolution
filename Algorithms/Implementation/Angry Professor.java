fbf7dd86e7877174f3f83bf51f0926b8f5a25367
PradheepKumarA <30936633+PradheepKumarA@users.noreply.github.com>
2017-08-14 01:13:05 +0530

    Update Angry Professor.java

--- Algorithms/Implementation/Angry Professor.java
+++ Algorithms/Implementation/Angry Professor.java
@@ -1,10 +1,10 @@
+/*
 import java.io.*;
 import java.util.*;
 
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
 		int[] N = new int[n];
@@ -34,10 +34,10 @@ public class Solution {
 		}
     }
 }
+*/
 
 
-Efficient
-
+//Efficient
 import java.io.*;
 import java.util.*;
 
